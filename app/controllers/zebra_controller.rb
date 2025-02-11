24572a02a69d30f432721b3acdfa031253cf3b31
jonathanzyin <jonathanyin@uchicago.edu>
2025-02-11 18:09:50 +0000

    finished

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,16 +1,22 @@
-class ZebraController < Application Controller
-  def Giraffe
+class ZebraController < ApplicationController
+  def monkey
+    render({ :template => "layouts/home"})
+  end
+
+  def giraffe
     @random_move = ["rock", "paper", "scissors"].sample
     render({ :template => "game_templates/play_rock"})
   end
 
-  def Elephant
+  def elephant
     @random_move = ["rock", "paper", "scissors"].sample
     render({ :template => "game_templates/play_paper"})
   end
 
-  def Lion
+  def lion
     @random_move = ["rock", "paper", "scissors"].sample
     render({ :template => "game_templates/play_scissors"})
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+get("/", { :controller => "zebra", :action => "monkey"})
+
 get("/rock", { :controller => "zebra", :action => "giraffe"} )
-end
+
 get("/paper", { :controller => "zebra", :action => "elephant"} )
-end
+
 get("/scissors", { :controller => "zebra", :action => "lion"} )
-end
+
 end
